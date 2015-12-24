02133e5f9a20b8216ae729a6491daca6720a1520
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2015-12-24 02:03:05 +0000

    deleted class file

--- ex02/Marks.java
+++ ex02/Marks.java
@@ -1,11 +1,5 @@
 public class Marks {
-	private static int studentCount = 0;
-	private static int distinctionCount = 0;
-	private static int passCount = 0;
-	private static int failCount = 0;
-	private static int invalidCount = 0;
-	private int mark;
-	private int count;
+	
 
 	public static void main(String[] args) {
 		Marks count = new Marks();
@@ -13,24 +7,32 @@ public class Marks {
 	}
 
 	public void markCounter(){
-			System.out.println("Enter marks <0-100> one per line");
-			System.out.println("Enter -1 when finished");
-			do {
-				System.out.print("Enter a mark: ");
-				mark = Integer.parseInt(System.console().readLine());
-				studentCount++;
-				if (mark >= 70 && mark <= 100) {
-					distinctionCount++;
-				} else if (mark >= 50 && mark <= 69) {
-					passCount++;
-				} else if (mark >= 0 && mark <= 49) {
-					failCount++;
-				} else if (mark >= 100 || mark < -1) {
-					invalidCount++;
-				}
-			} while (mark!=-1);
-			System.out.println("There are " + (studentCount-1) + " students: "
-					 + distinctionCount + " distinctions, " + passCount + " passes, "
-					  + failCount + " fails (plus " + invalidCount + " invalid).");
+		int studentCount = 0;
+		int distinctionCount = 0;
+		int passCount = 0;
+		int failCount = 0;
+		int invalidCount = -1;
+		int mark;
+		int count;
+		System.out.println("Enter marks <0-100> one per line");
+		System.out.println("Enter -1 when finished");
+		do {
+			System.out.print("Enter a mark: ");
+			mark = Integer.parseInt(System.console().readLine());
+			studentCount++;
+			if (mark >= 70 && mark <= 100) {
+				distinctionCount++;
+			} else if (mark >= 50 && mark <= 69) {
+				passCount++;
+			} else if (mark >= 0 && mark <= 49) {
+				failCount++;
+			} else if (mark >= 100 || mark < 0) {
+				studentCount--;
+				invalidCount++;
+			}
+		} while (mark != -1);
+		System.out.println("There are " + studentCount + " students: "
+				 + distinctionCount + " distinctions, " + passCount + " passes, "
+				  + failCount + " fails (plus " + invalidCount + " invalid)");
 	}
 }

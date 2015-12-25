51ed37cb1437c9d1c18d21f5cb7dbd2ecceb474c
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2015-12-25 00:55:39 +0000

    q06 - completed & tested

--- ex06/Patient.java
+++ ex06/Patient.java
@@ -4,71 +4,60 @@ public class Patient {
     private int age;
     private String illness;
     private Patient nextPatient;
-    private static int length = 0;
-    private Patient temp;
-    private boolean first;
+    private static int patientCount = 1;
 
-    public Patient(String name, int age, String illness, Patient start) {
-        this.name = name;
-        this.age = age;
-        this.illness = illness;
-        this.nextPatient = start;
-        this.first = false;
-    }
-
-    public Patient(String name, int age, String illness, boolean first) {
+    public Patient(String name, int age, String illness) {
         this.name = name;
         this.age = age;
         this.illness = illness;
         this.nextPatient = this;
-        this.first = true;
+    }
+
+    public static int getPatientCount() {
+        return patientCount;
+    }
+
+    // For updating patientCount when deleting first patient in list
+    public static void decPatientCount() {
+        patientCount--;
+    }
+
+    public Patient getNextPatient() {
+        return nextPatient;
+    }
+
+    public void setNextPatient(Patient p) {
+        nextPatient = p;
     }
 
     public void addPatient(Patient newPatient) {
-        if (nextPatient.first) {
-            this.nextPatient = newPatient;
-        } else {
-            this.nextPatient.addPatient(newPatient);
-        }
+        Patient temp = this;
+        while (true) {
+            if (temp.nextPatient == this) {
+                temp.nextPatient = newPatient;
+                newPatient.nextPatient = this;
+                patientCount++;
+                break;
+            } else {
+                temp = temp.nextPatient;
+            }
+        }  
     }
 
-    public boolean deletePatient(Patient patient) {
-        if (nextPatient.first) {
+    public boolean deletePatient(String name) {
+        if (nextPatient == this) {
             return false;
-        } else if (this.nextPatient.name.equals(patient.name)) {
-            this.nextPatient = nextPatient.nextPatient;
+        } else if (nextPatient.name.equals(name)) {
+            nextPatient = nextPatient.nextPatient;
+            System.out.println("Deleted " + name);
+            patientCount--;
             return true;
         } else {
-            return this.nextPatient.deletePatient(patient);
+            return nextPatient.deletePatient(name);
         }
     }
 
-    public void printList() {
-        System.out.println
-        (this.name + "\tNEXT: " + this.nextPatient.name);
-        if (!this.nextPatient.first) {
-                this.nextPatient.printList();
-        }
-    }
-
-    public void listLength() {
-        length++;
-        if (this.nextPatient.first) {
-            this.nextPatient.listLength();
-        } else {
-            System.out.println("\n(recursively) The length of the list is " + length);
-            length = 0;
-        }
-    }
-
-    public void iterativeLength() {
-        Patient temp = this;
-        length = 1;
-        do {
-            temp = temp.nextPatient;
-            length++;
-        } while (temp.nextPatient.first);
-        System.out.println("(iteratively) The length of the list is " + (length));
-        length = 0;
+    public String toString() {
+        return "Name: " + name + "\tAge: " + age + "\t\tIllness: " + illness;
     }
 }
